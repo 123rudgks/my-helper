e17b6a208b09e1d3a23b2437a56265bb1ad5f7fd
123rudgks <123rudgks@gmail.com>
2023-02-02 19:18:46 +0900

    feat : create folder

---
+++ helper/getFormDataFromObj.ts
@@ -0,0 +1,12 @@
+interface FormDataValueProps {
+  [key: string]: string | Blob;
+}
+export const getFormDataFromObj = <T extends FormDataValueProps>(obj: T) => {
+  const formData = new FormData();
+  Object.keys(obj).forEach((key) => {
+    const typedKey = key as keyof T;
+    const value = obj[typedKey];
+    formData.append(String(typedKey), value);
+  });
+  return formData;
+};
